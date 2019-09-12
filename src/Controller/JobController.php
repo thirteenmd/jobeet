5eb41fc6fbe28f6894bedea102b35cc84991101d
Andreea Cristina Cojocaru <acojocaru@pnentalog.fr>
2019-09-12 10:06:07 +0300

    Day 4: The controller and the View

---
+++ src/Controller/JobController.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Entity\Job;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("job")
+ */
+class JobController extends AbstractController
+{
+    /**
+     * @Route("/", name="job.list")
+     *
+     * @return Response
+     */
+    public function list(): Response
+    {
+        $jobs = $this->getDoctrine()->getRepository(Job::class)->findAll();
+
+        return $this->render('job/list/html.twig', [
+            'jobs' => $jobs,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="job.show")
+     *
+     * @param Job $job
+     *
+     * @return Response
+     */
+    public function show(Job $job) : Response
+    {
+        return $this->render('job/show.html.twig', [
+            'job' => $job,
+        ]);
+    }
+
+}
